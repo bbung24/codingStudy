819bc5420e6cb779e1e6bd70a41fbdf367b6231e
Inho Yong <bbung24@gmail.com>
2016-04-27 23:58:48 -0400

    Cracking the coding interview chapter 6 : Brain Teasers.

---
+++ myown/python/Chapter6/ch6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+This file is for cracking the coding interview Chapter 6 : Brain Teasers.
+'''
+# Examplify, Simplify, Generalize, Pattern matching, and Base Case and build.
+#------------------------------------------------------------------------------
+def findHeavyBottle():
+	'''
+	6.1
+	You have 20 bottles of pills. 19 bottles have 1.0 gram pills, but one has pills 
+	of weight 1.1 grams. Given a scale that provides an exact measurement, how
+	would you find the heavy bottle? You can only use the scale once.
+	'''
+	
