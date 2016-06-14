cd380db172b20aa67c073a5eace6ef7b9bcdaf2f
Inho Yong <bbung24@gmail.com>
2016-06-14 17:41:17 -0400

    CTCI
    6.1 The Heavy Pill.

--- ctci/myown/python/Chapter6/ch6.py
+++ ctci/myown/python/Chapter6/ch6.py
@@ -12,4 +12,9 @@ def findHeavyBottle():
 	of weight 1.1 grams. Given a scale that provides an exact measurement, how
 	would you find the heavy bottle? You can only use the scale once.
 	'''
-	
+	"""
+	Take one pill from first bottle. Take two pills from second bottle and so forth.
+	If the sum is 20.1, heavy bottle is first bottle. 20.2 = second, 21.0 = 10th,
+	22.0 = 20th, etc.
+	CORRECT
+	"""
