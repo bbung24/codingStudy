5a136abbf3707cfba0ff6070f52bc5dfa1abe8c8
Inho Yong <bbung24@gmail.com>
2016-06-14 22:00:04 -0400

    CTCI
    8.1 Triple Step.

--- ctci/myown/python/Chapter8/ch8.py
+++ ctci/myown/python/Chapter8/ch8.py
@@ -4,10 +4,36 @@
 This file is for cracking the coding interview Chapter 8 : Recursive and Dynamic Programming
 '''
 
+import unittest 
+
 #------------------------------------------------------------------------------
+def triple_step(n):
 	'''
-	8.1
-	Design the data structures for a generic deck of cards. Explain how you would
-	subclass the data structures to implement blackjack.
+	8.1 Triple Step
+	A child is running up a staircase with n steps and can hop either 1 step, 
+	2 steps, or 3 steps at a time. Implement a method to count how many possible
+	ways the child can run up the stairs.
 	'''
-	
+	cache = {1:1, 2:2, 3:4}
+	return triple_step_helper(n, cache)
+
+def triple_step_helper(n, cache):
+	if n in cache:
+		return cache[n]
+	else:
+		result = (triple_step_helper(n-1, cache) + triple_step_helper(n-2, cache) +
+			triple_step_helper(n-3, cache))
+		cache[n] = result
+		return result
+
+class TestClass(unittest.TestCase):
+	def test_triple_step1(self):
+		actual = triple_step(6)
+		self.assertEqual(24, actual)
+
+	def test_triple_step2(self):
+		actual = triple_step(5)
+		self.assertEqual(13, actual)
+
+if __name__ == "__main__":
+	unittest.main()
