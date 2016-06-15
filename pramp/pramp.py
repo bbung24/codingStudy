850363eba996d8cbbb98ca7d64912517e7c78add
Inho Yong <bbung24@gmail.com>
2016-06-15 02:26:23 -0400

    Pramp interview prep.
    Shifted Array Search

--- pramp/pramp.py
+++ pramp/pramp.py
@@ -100,13 +100,69 @@ def find_sum(arr, limit):
     return -1
 
 #------------------------------------------------------------------------------
+def shifted_array_search(arr, num):
+    """
+    Shifted Array Search
+
+    1. Find a given number num in a sorted array arr: --> Do Binary Search
+    arr = [2, 4, 5, 9, 12, 17] 
+
+    2. If the sorted array arr is shifted left by an unknown offset and 
+    you don't have a pre-shifted copy of it, how would you modify your method 
+    to find a number in the shifted array?
+    shiftArr = [9, 12, 17, 2, 4, 5]
+
+    Explain and code an efficient solution and analyze its runtime complexity
+    if num doesn't exist in the array, return -1
+    """
+    begin = 0
+    end = len(arr)-1
+    while begin < end:
+        mid = (begin + end) / 2
+        if arr[mid] == num:
+            return mid
+        elif arr[end] == num:
+            return end
+        elif arr[begin] == num:
+            return begin
+        if num > arr[mid]:
+            # Case where number looking for is greater than mid
+            if arr[end] > num: # if right end is greater than num, look right.
+                begin = mid + 1
+            elif arr[begin] > arr[mid]: # if there is shift in left side, look left.
+                end = mid - 1
+            else: # if there is shift in right side, still need to look right.
+                begin = mid + 1
+        elif num < arr[mid]: 
+            # Case where number looking for is less than mid
+            if arr[begin] < num: # if left begin is less than num look left.
+                end = mid - 1
+            elif arr[end] < arr[mid]: # if there is shift in right side, look right.
+                begin = mid + 1
+            else: # if there is shift in left side, still need to look left.
+                end = mid - 1
+    return -1
+
+#------------------------------------------------------------------------------
 class Test(unittest.TestCase):
+    def test_shifted_array_search(self):
+        shiftArr = [9, 12, 17, 2, 4, 5]
+        num = 12
+        actual = shifted_array_search(shiftArr, num)
+        self.assertEqual(1, actual)
+        num = 4
+        actual = shifted_array_search(shiftArr, num)
+        self.assertEqual(4, actual)
+        num = 3
+        actual = shifted_array_search(shiftArr, num)
+        self.assertEqual(-1, actual)
+
     def test_find_sum(self):
         arr = [1, 2, 3, 4]
         limit = 6
         actual = find_sum(arr, limit)
         self.assertEqual([1,3], actual)
-        
+
     def test_sent_rev1(self):
         arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
         expected = [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'e', 'r', 'f', 'e', 'c', 't' ]
