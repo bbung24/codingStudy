331a0abf01afd280671609e524056d0324e2839c
Inho Yong <bbung24@gmail.com>
2016-06-15 13:51:49 -0400

    CTCI
    16.1 Number Swapper

--- ctci/myown/python/Chapter16/ch16.py
+++ ctci/myown/python/Chapter16/ch16.py
@@ -4,10 +4,41 @@
 This file is for cracking the coding interview Chapter 16 : Moderate
 '''
 
+import unittest
 #------------------------------------------------------------------------------
+def num_swap(a, b):
 	'''
 	16.1 Number Swapper
 	Write a function to swap a number in place (that is, without temporary variables).
 	'''
-	
-	
+	if a > b:
+		a += b
+		b = a - b
+		a = a - b
+	else:
+		b += a
+		a = b - a
+		b = b - a
+	return a, b
+
+def num_swap_sol(a, b):
+	"""
+	Solution from the book uses xor operator
+	"""
+	a = a ^ b
+	b = a ^ b
+	a = a ^ b
+	return a, b
+
+#------------------------------------------------------------------------------
+class TestClass(unittest.TestCase):
+	def test_num_swap(self):
+		actual = num_swap(6, 4)
+		self.assertEqual((4, 6), actual)
+
+	def test_num_swap_sol(self):
+		actual = num_swap_sol(6, 4)
+		self.assertEqual((4, 6), actual)
+
+if __name__ == '__main__':
+	unittest.main()
