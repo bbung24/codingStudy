3d7512c61a5d494d0646bf0c5001a41a84b86f65
Inho Yong <bbung24@gmail.com>
2016-06-15 17:18:49 -0400

    CTCI
    8.2 Robot in a Grid

--- ctci/myown/python/Chapter8/ch8.py
+++ ctci/myown/python/Chapter8/ch8.py
@@ -26,7 +26,48 @@ def triple_step_helper(n, cache):
 		cache[n] = result
 		return result
 
+#------------------------------------------------------------------------------
+def robot_in_a_grid(grid):
+	"""
+	8.2 Robot in a Grid
+	Imagine a robot sitting on the upper left corner of grid with r rows and c 
+	columns. The robot can only move in two directions, right and down, but 
+	certain cells are "off limits" such that the robot cannot step on them.
+	Design an algorithm to find a path for the robot from the top left to the 
+	bottom right.
+	"""
+	result = []
+	robot_helper(grid, result, 0, 0)
+	return result
+
+def robot_helper(grid, result, row, col):
+	result.append((row,col))
+	if row == len(grid)-1 and col == len(grid[0])-1:
+		return True
+	if grid[row][col] == 1:
+		result.pop()
+		return False
+	elif col+1 < len(grid[0]) and robot_helper(grid, result, row, col+1):
+		return True
+	elif row+1 < len(grid) and robot_helper(grid, result, row+1, col):
+		return True
+	else:
+		result.pop()
+		return False
+"""
+In the solution, we can make this little faster by using cache for failed points
+"""
+
+#------------------------------------------------------------------------------
 class TestClass(unittest.TestCase):
+	def test_robot1(self):
+		grid = [[0,0,0],[0,0,0],[0,0,0]]
+		actual = robot_in_a_grid(grid)
+		self.assertEqual([(0,0), (0,1), (0,2), (1,2), (2,2)], actual)
+		grid = [[0,1,0],[0,0,0],[0,0,0]]
+		actual = robot_in_a_grid(grid)
+		self.assertEqual([(0,0), (1,0), (1,1), (1,2), (2,2)], actual)
+
 	def test_triple_step1(self):
 		actual = triple_step(6)
 		self.assertEqual(24, actual)
