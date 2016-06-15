02361847905b9aacf7d41314368ff1e18d95d82f
Inho Yong <bbung24@gmail.com>
2016-06-15 16:32:09 -0400

    CTCI
    6.2 Basketball

--- ctci/myown/python/Chapter6/ch6.py
+++ ctci/myown/python/Chapter6/ch6.py
@@ -18,3 +18,15 @@ def findHeavyBottle():
 	22.0 = 20th, etc.
 	CORRECT
 	"""
+
+"""
+6.2 Basketball
+You have a basketball hoop and someone says that you can play one of two games.
+Game 1: You get one shot to make the hoop.
+Game 2: You get three shots and you have to make two fo three shots.
+If p is the probability of making a particular shot, for which values of p should
+you pick one game or the other? 
+"""
+"""
+See Ipad for an answer.
+"""
