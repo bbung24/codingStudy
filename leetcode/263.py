83efa9c53dbd568f6fec0e76c699bf241c944614
Inho Yong <bbung24@gmail.com>
2016-06-23 01:17:19 -0400

    263. Ugly Number
    Cleaner and faster. runtime beats 39.05% of python submissions.

--- leetcode/263.py
+++ leetcode/263.py
@@ -18,12 +18,9 @@ def isUgly(num):
     """
     if num == 0:
         return False
-    while num % 2 == 0:
-        num /= 2
-    while num % 3 == 0:
-        num /= 3
-    while num % 5 == 0:
-        num /= 5
+    for n in (2,3,5):
+        while num % n == 0:
+            num /= n
     if num == 1:
         return True
     else:
