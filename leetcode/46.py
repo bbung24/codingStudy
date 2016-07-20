7d846bfde4a56cf94b53e34bde172bcee4746665
Inho Yong <bbung24@gmail.com>
2016-07-20 00:38:11 -0400

    46. Permutations
    Previous commit wasn't correct. It had edge case of not doing right
    when there is no bigger permutations and had to go back to lowest one.

--- leetcode/46.py
+++ leetcode/46.py
@@ -37,25 +37,52 @@ class Solution(object):
         """
         new = [x for x in nums]
         i = len(new)-1
+        largest = new[i]
         for index in range(len(new)-2, -1, -1):
-            if new[index] < new[index+1]:
+            if new[index] < largest:
                 i = index
                 break
-        else:
-            i = 0
+            largest = max(largest, new[index])
         j = len(new) -1
         for index in range(len(new)-1, i, -1):
             if new[index] > new[i]:
                 j = index
                 break
-        new[i], new[j] = new[j], new[i]
-        i += 1
+        if i == j:
+            i = 0
+        else:
+            new[i], new[j] = new[j], new[i]
+            i += 1
         j = len(new) -1
         while i < j:
             new[i], new[j] = new[j], new[i]
             i += 1
             j -= 1
         return new
+        # new = [x for x in nums]
+        # largest = float('-inf')
+        # for index in range(len(new)-1, -1, -1):
+        #     if new[index] < largest:
+        #         for j in range(len(new)-1, index, -1):
+        #             if new[index] < new[j]:
+        #                 new[index], new[j] = new[j], new[index]
+        #                 break
+        #         i = index+1
+        #         j = len(new)-1
+        #         while i < j:
+        #             new[i], new[j] = new[j], new[i]
+        #             i += 1
+        #             j -= 1
+        #         break
+        #     largest = max(largest, new[index])
+        # else:
+        #     i = 0
+        #     j = len(new)-1
+        #     while i < j:
+        #         new[i], new[j] = new[j], new[i]
+        #         i += 1
+        #         j -= 1
+        # return new
         
 import unittest
 class TestClass(unittest.TestCase):
@@ -65,13 +92,12 @@ class TestClass(unittest.TestCase):
 
     def test_permute(self):
         nums = [1,2,3]
-        expected = [
-            [1,2,3],
-            [1,3,2],
-            [2,1,3],
-            [2,3,1],
-            [3,1,2],
-            [3,2,1]
-        ]
+        expected = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        actual = self.s.permute(nums)
+        self.assertEqual(actual, expected)
+
+    def test_permute2(self):
+        nums = [0,-1,1]
+        expected = [[0,-1,1],[0,1,-1],[-1,0,1],[-1,1,0],[1,0,-1],[1,-1,0]]
         actual = self.s.permute(nums)
         self.assertEqual(actual, expected)
